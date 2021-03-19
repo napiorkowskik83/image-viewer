79d80414b91203736661b294b83f7c9ce6e4d7d3
napiorkowskik83 <napiorkowskik83@gmail.com>
2021-03-19 08:13:13 +0100

    Added image rename and bigger files upload possibility

--- src/main/java/com/task/imageviewer/MainView.java
+++ src/main/java/com/task/imageviewer/MainView.java
@@ -1,8 +1,12 @@
 package com.task.imageviewer;
 
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.upload.Receiver;
 import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 
 import java.io.File;
@@ -18,8 +22,14 @@ public class MainView extends VerticalLayout {
         createUploadFolderIfNotExists();
         setSizeFull();
 
+        Label infoLabel = new Label("Enter image name to enable upload:");
+        infoLabel.getStyle().set("font-weight", "bold");
+
+        TextField textField = new TextField();
+
         Upload upload = new Upload((Receiver) (filename, mimeType) -> {
-            File file = new File(new File(IMAGE_FOLDER), filename);
+            String uploadedFileName = textField.getValue() + filename.substring(filename.length()-4);
+            File file = new File(new File(IMAGE_FOLDER), uploadedFileName);
             try {
                 return new FileOutputStream(file);
             } catch (FileNotFoundException e1) {
@@ -27,8 +37,20 @@ public class MainView extends VerticalLayout {
                 return null;
             }
         });
+        upload.setVisible(false);
 
-        add(upload);
+        textField.setValueChangeMode(ValueChangeMode.EAGER);
+        textField.addValueChangeListener(e -> {
+            if(textField.getValue().length() > 0){
+                upload.setVisible(true);
+            } else{
+                upload.setVisible(false);
+            }
+        });
+
+        HorizontalLayout uploadLayout = new HorizontalLayout(infoLabel, textField, upload);
+        uploadLayout.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
+        add(uploadLayout);
     }
 
     private static File createUploadFolderIfNotExists() {
