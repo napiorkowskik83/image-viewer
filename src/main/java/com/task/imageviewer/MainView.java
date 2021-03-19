c1a03f2dee4c05a315c583deb6c0de5474305efe
napiorkowskik83 <napiorkowskik83@gmail.com>
2021-03-19 07:17:36 +0100

    Added upload component

--- src/main/java/com/task/imageviewer/MainView.java
+++ src/main/java/com/task/imageviewer/MainView.java
@@ -1,8 +1,41 @@
 package com.task.imageviewer;
 
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.upload.Receiver;
+import com.vaadin.flow.component.upload.Upload;
 import com.vaadin.flow.router.Route;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 @Route("")
 public class MainView extends VerticalLayout {
+    private static final String IMAGE_FOLDER = "uploaded-images";
+
+    public MainView() {
+
+        createUploadFolderIfNotExists();
+        setSizeFull();
+
+        Upload upload = new Upload((Receiver) (filename, mimeType) -> {
+            File file = new File(new File(IMAGE_FOLDER), filename);
+            try {
+                return new FileOutputStream(file);
+            } catch (FileNotFoundException e1) {
+                e1.printStackTrace();
+                return null;
+            }
+        });
+
+        add(upload);
+    }
+
+    private static File createUploadFolderIfNotExists() {
+        File folder = new File(IMAGE_FOLDER);
+        if (!folder.exists()) {
+            folder.mkdirs();
+        }
+        return folder;
+    }
 }
